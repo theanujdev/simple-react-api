5fda15e5a805f74133ebc19230a74f07790844f0
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-07 22:59:44 +0530

    completed robots page with input validation also

--- src/components/RobotsPage.jsx
+++ src/components/RobotsPage.jsx
@@ -1,36 +1,54 @@
 import { useState } from "react";
 
-function Images(props) {
-  let numbers = Array(props.numbers).fill();
-  let listItems = numbers.map((_, i) => (
-    <img key={i} src="https://robohash.org/02" alt="" />
-  ));
-  return <>{listItems}</>;
+function Img({ imgNo }) {
+  let intNumber = parseInt(imgNo);
+  if (imgNo && Number.isInteger(intNumber) && intNumber > 0) {
+    return Array(intNumber)
+      .fill()
+      .map(() => {
+        let random = Math.floor(Math.random() * 1000) + 1;
+        return (
+          <img
+            src={`https://robohash.org/${random}`}
+            key={random}
+            alt="Robots"
+          />
+        );
+      });
+  }
+
+  return null;
 }
 
 const RobotsPage = () => {
-  const [imgNo, setImgNo] = useState(0);
+  const [imgNo, setImgNo] = useState(null);
   return (
-    <section class="p-10">
+    <section class="p-5 sm:p-10">
       <div class="container mx-auto text-center">
-        <div class="flex sm:w-2/3 mx-auto justify-between">
-          <h1 class="inline-block text-2xl sm:text-4xl">Robots</h1>
+        <div class="flex sm:w-2/3 mx-auto justify-evenly sm:justify-between">
+          <h1
+            class="inline-block font-bold
+           text-2xl sm:text-4xl"
+          >
+            Robots
+          </h1>
           <input
             class="bg-white shadow-sm text-lg sm:text-2xl px-3 w-1/2"
+            type="number"
             name="search"
             id="search"
+            min="0"
+            value={imgNo}
+            max="30"
+            autoComplete="off"
             placeholder="Number"
             onChange={(e) => {
               setImgNo(e.target.value);
             }}
           />
         </div>
-        <div class="grid grid-cols-3 md:grid-cols-5 justify-items-center mt-11 gap-10 w-3/4 mx-auto">
-          <Images numbers={5} />
-          <img src="https://dummyimage.com/100.jpg" alt="" />
-          <img src="https://dummyimage.com/100.jpg" alt="" />
-
-          <img src="https://dummyimage.com/100.jpg" alt="" />
+        <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 justify-items-center mt-11 gap-10 w-full md:w-11/12 mx-auto">
+          <Img imgNo={imgNo} />
         </div>
       </div>
     </section>
