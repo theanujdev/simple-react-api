2554a51e8b1d86d120691202f0b94b7b4f746acd
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-11 23:22:40 +0530

    photos page initial setup

--- src/components/PhotosPage.jsx
+++ src/components/PhotosPage.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import loadingBar from "../assets/img/loading.gif";
+import loadingRocket from "../assets/img/rocket.gif";
+
 function PhotosPage() {
-  return <div>Photos</div>;
+  const [loading, setLoading] = useState(true);
+  return (
+    <section class="p-10 bg-white">
+      <div class="text-center">
+        <h1 class="text-4xl">View Our Gallery</h1>
+      </div>
+      {loading && (
+        <div className="m-10">
+          <img class="block mx-auto" src={loadingRocket} alt="Loading" />
+        </div>
+      )}
+      <div class="flex flex-wrap justify-center p-7">
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+        <img class="m-7" src="https://dummyimage.com/200.jpg" alt="Image" />
+      </div>
+      <div className="m-10">
+        <img class="block mx-auto" src={loadingBar} alt="Loading" />
+      </div>
+    </section>
+  );
 }
 
 export default PhotosPage;
