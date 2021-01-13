765694860653703ccde970fa4bf6fae970277f2b
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-13 20:57:02 +0530

    switch route shifted to App.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import IpDetailsPage from "./components/IpDetailsPage";
+import JokesPage from "./components/JokesPage";
+import RobotsPage from "./components/RobotsPage";
+import PhotosPage from "./components/PhotosPage";
 
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/ip-details" component={IpDetailsPage} />
+          <Route path="/jokes" component={JokesPage} />
+          <Route path="/robots" component={RobotsPage} />
+          <Route path="/photos" component={PhotosPage} />
+        </Switch>
+      </Router>
     </>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,30 @@
-import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
-import HomePage from "./HomePage";
-import IpDetailsPage from "./IpDetailsPage";
-import JokesPage from "./JokesPage";
-import RobotsPage from "./RobotsPage";
-import PhotosPage from "./PhotosPage";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <Router>
-      <nav className="bg-purple-800 text-sm sm:text-2xl">
-        <div className="container mx-auto flex justify-evenly items-center py-3">
-          <Link to="/ip-details">
-            <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
-              IP Details
-            </button>
-          </Link>
-          <Link to="/jokes">
-            <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
-              Jokes
-            </button>
-          </Link>
-          <Link to="/robots">
-            <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
-              Robots
-            </button>
-          </Link>
-          <Link to="/photos">
-            <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
-              Photos
-            </button>
-          </Link>
-        </div>
-      </nav>
-      <Switch>
-        <Route path="/" exact component={HomePage} />
-        <Route path="/ip-details" component={IpDetailsPage} />
-        <Route path="/jokes" component={JokesPage} />
-        <Route path="/robots" component={RobotsPage} />
-        <Route path="/photos" component={PhotosPage} />
-      </Switch>
-    </Router>
+    <nav className="bg-purple-800 text-sm sm:text-2xl">
+      <div className="container mx-auto flex justify-evenly items-center py-3">
+        <Link to="/ip-details">
+          <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
+            IP Details
+          </button>
+        </Link>
+        <Link to="/jokes">
+          <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
+            Jokes
+          </button>
+        </Link>
+        <Link to="/robots">
+          <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
+            Robots
+          </button>
+        </Link>
+        <Link to="/photos">
+          <button className="text-white hover:shadow-md hover:bg-purple-600 focus:outline-none px-2 py-2 rounded-md">
+            Photos
+          </button>
+        </Link>
+      </div>
+    </nav>
   );
 };
 
