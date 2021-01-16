c1566f3af7f9eacba17d87ca7daffd6a75595033
Anuj Sharma <69591862+ProdexOne@users.noreply.github.com>
2021-01-16 22:58:11 +0530

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  purge: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +9,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
